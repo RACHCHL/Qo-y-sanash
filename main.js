442320edcbebd91d84f0b17f54c45c7fdd4cbaae
Rasulbek <quvondiqovrasulbek054@gmail.com>
2024-12-05 23:54:46 +0500

    Rachchl

---
+++ main.js
@@ -0,0 +1,12 @@
+let nam = prompt ("Son kiriting")
+if(isNaN(nam)){
+    alert("iltimos raqam kiriting")
+}
+let war = 1
+for (let i = 1; i <= nam; ) {
+    war= i++;
+    console.log(war + "  ta qo'y");
+    
+    
+}
+
